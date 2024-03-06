3d9ec6b2f8ff5a93f181fcb24648ce167cb85e5e
COrtega <carlos.ortega@sjsu.edu>
2024-03-06 13:29:54 -0800

    added screens to Root Navigator. No longer needed, but keep for record purposes.

--- navigators/RootNavigator.js
+++ navigators/RootNavigator.js
@@ -1,19 +1,27 @@
 import * as React from "react";
 
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Onboarding from "../screens/Onboarding";
 import Profile from "../screens/Profile";
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
-  return (
-    <Stack.Navigator initialRouteName="Onboarding">
-      <Stack.Screen name="Onboarding" component={Onboarding} options={{ title: 'Onboarding', headerTitleAlign: 'center' }}/>
-      <Stack.Screen name="Profile" component={Profile} options={{ title: 'Profile', headerTitleAlign: 'center' }} />
-    </Stack.Navigator>
+    return (
+        <Stack.Navigator initialRouteName="Onboarding">
+            <Stack.Screen
+                name="Onboarding"
+                component={Onboarding}
+                options={{
+                    title: 'Onboarding',
+                    headerTitleAlign: 'center',
+                    headerStyle: {backgroundColor: '#495e57'}
+                }}
+            />
+            <Stack.Screen name="Profile" component={Profile} options={{ title: 'Profile', headerTitleAlign: 'center' }} />
+        </Stack.Navigator>
 
-  );
+    );
 };
 
 export default RootNavigator;
