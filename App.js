db79558765148eff28be646e4eac421b38621dfc
COrtega <carlos.ortega@sjsu.edu>
2024-03-06 13:36:18 -0800

    added rootNavigator code to App.js. added onboarding logic for auth flow

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
+import React, { useEffect, useState } from 'react';
 import { PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from "@react-navigation/native";
-import RootNavigator from "./navigators/RootNavigator";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import RootNavigator from "./navigators/RootNavigator";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Onboarding from './screens/Onboarding';
+import Profile from './screens/Profile';
+// import SplashScreen from './screens/SplashScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const[isOnboardingCompleted, setOnboardingStatus] = useState(false)
+  const[isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const checkOnboardingStatus = async () => {
+        try {
+          const value = await AsyncStorage.getItem('onboardingCompleted');
+          if(value !== null) {
+            setOnboardingStatus(JSON.parse(value));
+          }
+          
+        } catch (e) {
+            console.log(e)
+        }
+        setIsLoading(false)
+    }
+
+    checkOnboardingStatus();
+  }, [])
+
+  // if (isLoading) {
+  //   // We haven't finished reading from AsyncStorage yet
+  //   return <SplashScreen />;
+  // }
   return (
     <PaperProvider>
       <NavigationContainer>
-        <RootNavigator/>
+        <Stack.Navigator>
+          {isOnboardingCompleted ? (
+            <Stack.Screen name="Profile" component={Profile} />
+          ) : (
+            <Stack.Screen name="Onboarding" component={Onboarding} />
+          )}
+        </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
   );
