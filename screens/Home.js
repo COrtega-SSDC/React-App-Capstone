4cc5af9428009c16555c8430516f387deedf5c91
COrtega-SSDC <carlos.ortega496@gmail.com>
2024-04-04 17:58:40 -0700

    created custom Header with styling

--- screens/Home.js
+++ screens/Home.js
@@ -3,19 +3,117 @@ import { useState, useEffect } from 'react';
 import { useFonts } from 'expo-font';
 import { Karla_400Regular } from '@expo-google-fonts/karla';
 import { MarkaziText_500Medium } from '@expo-google-fonts/markazi-text';
+import { View, Image, Text, StyleSheet } from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function Home() {
+export function HomeHeader() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
-    let [fontsLoaded, fontError] = useFonts({
-        Karla_400Regular,
-        MarkaziText_500Medium,
-      });
+  const [image, setImage] = useState(null);
 
-      if (!fontsLoaded && !fontError) {
-        return null;
-      }
-    
+  const getData = async () => {
+    try {
+      const keys = ['firstName', 'lastName'];
 
+      const values = await AsyncStorage.multiGet(keys);
+      const storedData = Object.fromEntries(values);
+
+      setFirstName(storedData.firstName || '');
+      setLastName(storedData.lastName || '');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const renderInitials = (firstName, lastName) => {
+    const firstInitial = firstName.length > 0 ? firstName[0] : '';
+    const lastInitial = lastName.length > 0 ? lastName[0] : '';
+    return firstInitial + lastInitial;
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <View style={styles.header}>
+      <View style={styles.leftContainer}></View>
+      <View style={styles.logoContainer}>
+        <Image source={require('../assets/LogoTest.png')} style={styles.logo} />
+      </View>
+      
+      <View style={styles.profile}>
+        {image ? (
+          <Image source={{ uri: image }} style={{ width: 179, height: 76 }} />
+        ) : (
+          <View style={styles.initialsAvatar}>
+            <Text style={styles.initialsText}>
+              {renderInitials(firstName, lastName)}
+            </Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
 }
+
+export default function Home() {
+  let [fontsLoaded, fontError] = useFonts({
+    Karla_400Regular,
+    MarkaziText_500Medium,
+  });
+
+  useEffect(() => {
+    fetch(
+      'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json'
+    ).then();
+  }, []);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <View>
+      <Text>Homepage</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 15
+  },
+  logoContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 80
+  },
+  initialsAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 36,
+    backgroundColor: '#00BCD4',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  initialsText: {
+    color: 'white',
+    fontSize: 19,
+    fontWeight: 'bold',
+  },  
+  logo: {
+    width: 179,
+    height: 76,
+    resizeMode: 'contain',
+  },
+  profile: {
+    marginRight: 25
+  },
+});
